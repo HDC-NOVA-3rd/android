aaeed0d5596d4aef026f2356e400bd0d259dfbb2
wnsrlf0721 <wnsrlf0721@ajou.ac.kr>
2026-02-13 19:13:18 +0900

    add google-services.json in secrets file

--- app.config.js
+++ app.config.js
@@ -1,6 +1,20 @@
 import "dotenv/config"; // .env 파일을 읽어오기 위해 필요
+import fs from "fs";
+import path from "path";
 
 export default ({ config }) => {
+  const googleServicesBase64 = process.env.GOOGLE_SERVICES_JSON_BASE64;
+  // EAS Build 환경에서 Base64 값이 있으면 파일을 생성합니다.
+  if (googleServicesBase64) {
+    const filePath = path.resolve(__dirname, "google-services.json");
+    fs.writeFileSync(filePath, Buffer.from(googleServicesBase64, "base64"));
+
+    // 생성된 파일을 사용하도록 경로 설정
+    if (config.android) {
+      config.android.googleServicesFile = "./google-services.json";
+    }
+  }
+
   return {
     ...config, // 기존 app.json에 있는 내용을 기본으로 가져옵니다.
 
